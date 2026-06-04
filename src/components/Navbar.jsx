ff426d00eda6d7f169ab8aa9088ff0fa7878803f
Fadhlan <fadhlanlim@gmail.com>
2026-06-04 09:53:09 +0700

    make a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from "react-router-dom";
+
+function Navbar() {
+  const location = useLocation();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">📄 CV Builder</Link>
+      </div>
+
+      <div className="navbar-links">
+        <Link
+          to="/"
+          className={location.pathname === "/" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </Link>
+        <Link
+          to="/builder"
+          className={
+            location.pathname === "/builder" ? "nav-link active" : "nav-link"
+          }
+        >
+          Builder
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/builder" element={<App />} />
